7ac1ad130aafc4566e0b9be8c44333e751ad4ea5
Tim-Gaede <TimGaede@gmail.com>
2019-09-24 23:10:12 -0400

    Update function textFactors!(n, primes).jl

--- function textFactors!(n, primes).jl
+++ function textFactors!(n, primes).jl
@@ -2,7 +2,7 @@ using Formatting
 
 #-------------------------------------------------------------------------------
 function superscript(n::Int)
-    if n == 0;    return "⁰"; end
+    if n == 0;    "⁰";    end
 
     res = ""
     sups = ["¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹", "⁰"]
@@ -21,9 +21,8 @@ end
 
 #-------------------------------------------------------------------------------
 function textFactors!(n::Int, primes::Array{Int,1})
-# Returns a sorted list of all divisors of n.
 # Will extend the array of primes if possibly inadequate for factorization
-    if n  < 2;    throw("n must be greater than one."); end
+    if n < 2;    throw("n must be greater than one."); end
 
     factors = Int64[]
     pwrs  = Int64[]
@@ -76,6 +75,82 @@ end
 
 
 #-------------------------------------------------------------------------------
+function textFactors!(nums::Array{Int,1}, primes::Array{Int,1})
+# Will extend the array of primes if possibly inadequate for factorization
+
+    # Get the maximum width of the formatted number in nums .................
+    nDigits_max = 0
+    for n in nums
+        nDigits = convert(Int, floor(log10(n))) + 1
+        if nDigits > nDigits_max
+            nDigits_max = nDigits
+        end
+    end
+    nCommas_max = (nDigits_max - 1) ÷ 3
+    pad = nDigits_max + nCommas_max
+    # .......................................................................
+
+    res = ""
+    for n in nums
+        n_fmd = lpad(format(n, commas=true), pad)
+
+        if n < 2
+            res *= n_fmd * "   has no prime factors"
+        end
+
+        factors = Int64[]
+        pwrs  = Int64[]
+
+        # Extend the array of primes if it may be inadequate
+        sqrt_n_up = convert(Int64, ceil(√n))
+        if sqrt_n_up > last(primes)
+            primes′ = primesTo(2sqrt_n_up)
+            for i = length(primes) + 1 : length(primes′)
+                push!(primes, primes′[i])
+            end
+        end
+
+        rem = n
+        sqrt_rem_down = convert(Int64, floor(√rem))
+        i = 1
+        while rem ≠ 1    &&    primes[i] ≤ sqrt_rem_down
+            if rem % primes[i] == 0
+
+                push!(factors, primes[i])
+                pwr = 0
+                while rem % primes[i] == 0
+                    rem ÷= primes[i]
+                    pwr += 1
+                end
+                push!(pwrs, pwr)
+            end
+            sqrt_rem_down = convert(Int64, floor(√rem)) # Speed up or slow down?
+            i += 1
+        end
+        if rem ≠ 1
+            push!(factors, rem)
+            push!(pwrs, 1)
+        end
+
+
+        res *= n_fmd * " = "
+        for i = 1 : length(factors) - 1
+            f_fmd = format(factors[i], commas=true)
+            res *= f_fmd * superscript(pwrs[i]) * " × "
+        end
+        res *= format(last(factors), commas=true) * superscript(last(pwrs))
+        res *= "\n"
+    end
+
+
+
+    res # returned
+end
+#-------------------------------------------------------------------------------
+
+
+
+#-------------------------------------------------------------------------------
 function primesTo(n::Integer)
     if n < 2;    return []; end
     primes = Int64[2]
@@ -101,8 +176,13 @@ end
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function main()
+    println("\n", "-"^50, "\n")
     primes = primesTo(7)
-    println(textFactors!(240051200, primes))
+
+
+    println(textFactors!(240_051_200, primes))
+    nums = [5040, 7680, 10_000, 10_240, 143_496_441, 240_051_200, 1517]
+    print(textFactors!(nums, primes)) 
 end
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 main()
